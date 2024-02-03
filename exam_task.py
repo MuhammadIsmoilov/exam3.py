ce7e518ede0cd7ccf9cfb3e24f1d4711c21aefde
MuhammadIsomoilov <ismoilovmuhammad247.com>
2024-02-03 20:10:13 +0500

    first commit

---
+++ exam_task.py
@@ -0,0 +1,29 @@
+# def power(n,x):                   misoli 1   
+#     if x == 0:
+#         return 1
+#     else:
+#       return n * pow(n,x-1)
+# n = int(input())
+# x = int(input())
+# power(n,x)    
+
+
+
+# def pow(n):                         
+#      dict = {}               misoli4
+#      for i in range(1,n+1):
+#           dict[i] = i * i
+#      return dict
+# print(pow(5))
+
+
+
+    
+
+
+
+
+
+
+
+
